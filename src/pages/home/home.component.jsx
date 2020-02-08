eece0439c52c9cad8d79bb1e04253599cff0f6a8
August Jr <iamranchojr@gmail.com>
2020-02-08 01:19:13 +0000

    Fully implemented

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,7 +2,14 @@
 import React from "react";
 
 // import carbon components
-import { Loading } from "carbon-components-react";
+import {
+  Loading,
+  Checkbox,
+  Select,
+  SelectItem,
+  Search,
+  TextInput
+} from "carbon-components-react";
 
 // import components
 import MovieCard from "../../components/movie-card/movie-card.component";
@@ -25,7 +32,10 @@ class Home extends React.Component {
       loading: true,
       errorMessage: null,
       movies: [],
-      genres: []
+      genres: [],
+      genreFilter: [],
+      rating: "",
+      query: ""
     };
   }
 
@@ -59,24 +69,67 @@ class Home extends React.Component {
     }
 
     // success, update state with movies
-    this.setState(
-      {
-        movies: movieResponse.movies,
-        genres: genreResponse.genres,
-        loading: false
-      },
-      () => {
-        console.log(this.state.movies);
-        console.log(this.state.genres);
-      }
-    );
+    this.setState({
+      movies: movieResponse.movies,
+      genres: genreResponse.genres,
+      loading: false
+    });
   };
 
   // override render to return html
   render() {
     let movies = this.state.movies;
+    let movieGenreIds = [];
 
-    // TODO: Filter
+    // select genres that belongs to movies
+    movies.forEach(movie => {
+      movieGenreIds.push(...movie.genre_ids);
+    });
+
+    // make sure they are no duplicates in the ids
+    movieGenreIds = [...new Set(movieGenreIds)];
+
+    // now populate genres based on the distinct ids we have
+    const genres = [];
+    movieGenreIds.forEach(genreId =>
+      genres.push(this.state.genres.find(genre => genre.id === genreId))
+    );
+
+    // Filter of genre if any selected
+
+    if (this.state.genreFilter.length > 0) {
+      const filteredMoviesByGenre = [];
+      this.state.genreFilter.forEach(genreId => {
+        const filterMovies = movies.filter(movie =>
+          movie.genre_ids.includes(genreId)
+        );
+        console.log(filterMovies);
+
+        filterMovies.forEach(movie => {
+          // add only if it does not already exist
+          if (!filteredMoviesByGenre.find(m => m.id === movie.id)) {
+            filteredMoviesByGenre.push(movie);
+          }
+        });
+      });
+
+      movies = filteredMoviesByGenre;
+    }
+
+    // filter by rating if valid
+    if (this.state.rating.length > 0 && this.state.rating <= 10) {
+      movies = movies.filter(movie => movie.vote_average == this.state.rating);
+    }
+
+    // filter by query if not empty
+    if (this.state.query.length > 0) {
+      movies = movies.filter(movie =>
+        movie.title.toLowerCase().includes(this.state.query.toLowerCase())
+      );
+    }
+
+    // sort by popularity
+    movies.sort((a, b) => b.popularity - a.popularity);
 
     return (
       <div className="movie-listings">
@@ -99,6 +152,65 @@ class Home extends React.Component {
         </div>
 
         <div className="movie-list" hidden={this.state.loading}>
+          <div className="genre-filter mb-10">
+            <p className="mb-10 font-bold">Filter</p>
+            <div className="bx--row">
+              {genres.map(genre => (
+                <div key={genre.id} className="bx--col-md-1">
+                  <Checkbox
+                    id={`${genre.id}`}
+                    labelText={genre.name}
+                    onChange={value => {
+                      const genreFilter = [...this.state.genreFilter];
+                      if (value) {
+                        // add
+                        genreFilter.push(genre.id);
+                      } else {
+                        // remove
+                        const index = genreFilter.findIndex(
+                          genreId => genreId === genre.id
+                        );
+
+                        genreFilter.splice(index, 1);
+                      }
+
+                      this.setState({
+                        genreFilter: genreFilter
+                      });
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="rating-search-filter mb-20">
+            <div className="bx--row">
+              <div className="bx--col-lg-2">
+                <TextInput
+                  data-invalid={this.state.rating > 10}
+                  type="number"
+                  min={0}
+                  max={10}
+                  step={0.5}
+                  value={this.state.rating}
+                  placeholder="Rating"
+                  onChange={e => {
+                    this.setState({ rating: e.target.value });
+                  }}
+                />
+              </div>
+              <div className="bx--col-lg-10">
+                <Search
+                  value={this.state.query}
+                  placeHolderText="Search by movie title"
+                  onChange={e => {
+                    this.setState({ query: e.target.value });
+                  }}
+                />
+              </div>
+            </div>
+          </div>
           <div className="bx--row">
             {movies.map(movie => (
               <div key={movie.id} className="bx--col-lg-3 mb-20 fade-in">
